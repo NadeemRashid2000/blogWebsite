d173a643a05fe498ef57832df5f3fcf75ea01a7f
NadeemRashid2000 <nrt192121@gmail.com>
2025-07-26 09:29:33 +0530

    use .env.production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // console.log(" API_BASE_URL in use:", API_BASE_URL);  //  Debug log
 
